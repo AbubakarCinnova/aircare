d4d033758bab13e222fced32ed6b8502cc14a0b4
AbubakarCinnova <130035999+AbubakarCinnova@users.noreply.github.com>
2023-09-06 14:31:35 +0500

    Update footer.php

--- footer.php
+++ footer.php
@@ -5,8 +5,8 @@
             <div class="col-md-4 col-sm-12 col-xs-12 footer-item" style="text-align:center;">
 
                 <!--   <h3>About Us</h3> -->
-                <p><img src="<?php echo $base_url ?>/img/premier dealer (2).jpg" style="width:70%"></p>
-                <p><img src="<?php echo $base_url ?>/img/kohler gen logo.jpg" style="width:200px"></p>
+                <p><img src="<?php echo $base_url ?>/img/premier dealer (2) (1).jpg" style="width:70%"></p>
+                <!--   <p><img src="<?php echo $base_url ?>/img/kohler gen logo.jpg" style="width:200px"></p> -->
                 <!--     <p class="light">At Jack's Heating and Cooling, we aim to exceed client expectations by employing the
                     best customer service people and holding ourselves to only the highest level of customer care. Our
                     friendly customer service people will answer all your questions, scheduling service, financing and
