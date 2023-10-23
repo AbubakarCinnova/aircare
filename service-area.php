5cd2b4c6d7dd4701bff62dfe5dfcf8c767b1113f
AbubakarCinnova <amahboob@cinnova.com>
2023-10-23 17:05:24 +0500

    Update service-area.php

--- service-area.php
+++ service-area.php
@@ -15,143 +15,148 @@
                         <div class="row">
                             <div class="col-lg-12">
 
-                                <h1>Advance Aircare Service Area</h1>
+                                <<h1>Advance Air Care Service Area</h1>
 
-                                <div class="service-container">
-                                    <div class="service-list margin-bottom-md">
-                                        <strong>All of Macomb County, MI Including:</strong>
-                                        <ul>
+                                    <div class="service-container">
+                                        <div class="service-list margin-bottom-md">
+                                            <strong>All of Macomb County, MI Including:</strong>
+                                            <ul>
 
-                                            <li><a
-                                                    href="<?php echo $base_url ?>/service-area/roseville.php">Roseville</a>
-                                            </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/clair-shores.php">St.
-                                                    Clair Shores</a></li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/sterling-heights.php">Sterling
-                                                    Heights</a></li>
-                                            <li>Utica</li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/warren.php">Warren</a>
-                                            </li>
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/roseville.php">Roseville</a>
+                                                </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/clair-shores.php">St.
+                                                        Clair Shores</a></li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/sterling-heights.php">Sterling
+                                                        Heights</a></li>
+                                                <li>Utica</li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/warren.php">Warren</a>
+                                                </li>
 
-                                            <li><a href="<?php echo $base_url ?>/service-area/harrison-township.php">
-                                                    Harrison Township</a>
-                                            </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/shelby-township.php">Shelby
-                                                    Township</a>
-                                            </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/macomb.php">Macomb
-                                                    Township</a> </li>
-                                            <li>Chesterfield
-                                                Township </li>
-                                        </ul>
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/harrison-township.php">
+                                                        Harrison Township</a>
+                                                </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/shelby-township.php">Shelby
+                                                        Township</a>
+                                                </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/macomb.php">Macomb
+                                                        Township</a> </li>
+                                                <li>Chesterfield
+                                                    Township </li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="service-list margin-bottom-md">
+                                            <strong>All of Oakland County, MI Including: </strong>
+                                            <ul>
+
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/birmingham.php">Birmingham</a>
+                                                </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/bloomfield-hills.php">Bloomfield
+                                                        Hills</a></li>
+                                                <li>Clarkston
+                                                </li>
+
+                                                <li><a href="<?php echo $base_url ?>/service-area/farmington-hills.php">Farmington
+                                                        Hills</a></li>
+
+                                                <li><a href="<?php echo $base_url ?>/service-area/ferndale.php">
+                                                        Ferndale</a></li>
+
+                                                <li><a href="<?php echo $base_url ?>/service-area/madison-heights.php">
+                                                        Madison Heights</a></li>
+
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/rochester.php">Rochester</a>
+                                                </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/rochester-hills.php">Rochester
+                                                        Hills</a></li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/royal-oak.php">Royal
+                                                        Oak</a></li>
+
+                                                <li><a href="<?php echo $base_url ?>/service-area/troy.php">Troy</a>
+                                                </li>
+
+                                                <li>Lake
+                                                    Orion</li>
+
+                                                <li> <a
+                                                        href="<?php echo $base_url ?>/service-area/waterford-township.php">Waterford
+                                                        Township</a></li>
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/independence-township.php">Independence
+                                                        Township</a></li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/oakland-township.php">Oakland
+                                                        Township</a> </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/west-bloomfield.php">West
+                                                        Bloomfield Township</a></li>
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/bloomfield-township.php">Bloomfield
+                                                        Township</a>
+                                                </li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="service-list margin-bottom-md">
+                                            <strong>Parts of Wayne County, MI Including: </strong>
+                                            <ul>
+
+                                                <li>Grosse
+                                                    Pointe Farms </li>
+                                                <li><a href="<?php echo $base_url ?>/service-area/grosse-pointe.php">Grosse
+                                                        Pointe</a> </li>
+
+                                                <li><a
+                                                        href="<?php echo $base_url ?>/service-area/dearborn.php">Dearborn</a>
+                                                </li>
+                                                <li> Dearborn Heights</li>
+                                                <li> Grosse Pointe </li>
+                                                <li> Grosse Pointe Park</li>
+                                                <li> Grosse Pointe Shores</li>
+                                                <li> Grosse Pointe Woods</li>
+                                                <li> Harper Woods</li>
+                                                <li> Livonia</li>
+                                                <li> Northville </li>
+                                                <li> Plymouth</li>
+                                                <li> Canton Township</li>
+                                                <li> Northville Township</li>
+                                                <li> Plymouth Township</li>
+                                            </ul>
+                                        </div>
+
+                                        <div class="service-list margin-bottom-md">
+
+                                            <strong>Parts of St. Clair County, MI </strong>
+                                            <ul>
+                                                <li>Call for more information</li>
+
+                                            </ul>
+                                        </div>
+
+                                        <div class="service-list margin-bottom-md">
+
+                                            <strong>Parts of Lapeer County, MI</strong>
+                                            <ul>
+                                                <li>Call for more information</li>
+
+                                            </ul>
+                                        </div>
+
+                                        <div class="service-list margin-bottom-md">
+
+                                            <strong>Parts of Livingston County, MI</strong>
+                                            <ul>
+                                                <li>Call for more information</li>
+
+                                            </ul>
+                                        </div>
+
+
+
+
                                     </div>
-
-                                    <div class="service-list margin-bottom-md">
-                                        <strong>All of Oakland County, MI Including: </strong>
-                                        <ul>
-
-                                            <li><a
-                                                    href="<?php echo $base_url ?>/service-area/birmingham.php">Birmingham</a>
-                                            </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/bloomfield-hills.php">Bloomfield
-                                                    Hills</a></li>
-                                            <li>Clarkston
-                                            </li>
-
-                                            <li><a href="<?php echo $base_url ?>/service-area/farmington-hills.php">Farmington
-                                                    Hills</a></li>
-
-                                            <li><a href="<?php echo $base_url ?>/service-area/ferndale.php">
-                                                    Ferndale</a></li>
-
-                                            <li><a href="<?php echo $base_url ?>/service-area/madison-heights.php">
-                                                    Madison Heights</a></li>
-
-                                            <li><a
-                                                    href="<?php echo $base_url ?>/service-area/rochester.php">Rochester</a>
-                                            </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/rochester-hills.php">Rochester
-                                                    Hills</a></li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/royal-oak.php">Royal
-                                                    Oak</a></li>
-
-                                            <li><a href="<?php echo $base_url ?>/service-area/troy.php">Troy</a></li>
-
-                                            <li>Lake
-                                                Orion</li>
-
-                                            <li> <a href="<?php echo $base_url ?>/service-area/waterford-township.php">Waterford
-                                                    Township</a></li>
-                                            <li><a
-                                                    href="<?php echo $base_url ?>/service-area/independence-township.php">Independence
-                                                    Township</a></li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/oakland-township.php">Oakland
-                                                    Township</a> </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/west-bloomfield.php">West
-                                                    Bloomfield Township</a></li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/bloomfield-township.php">Bloomfield
-                                                    Township</a>
-                                            </li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="service-list margin-bottom-md">
-                                        <strong>Parts of Wayne County, MI Including: </strong>
-                                        <ul>
-
-                                            <li>Grosse
-                                                Pointe Farms </li>
-                                            <li><a href="<?php echo $base_url ?>/service-area/grosse-pointe.php">Grosse
-                                                    Pointe</a> </li>
-
-                                            <li><a href="<?php echo $base_url ?>/service-area/dearborn.php">Dearborn</a>
-                                            </li>
-                                            <li> Dearborn Heights</li>
-                                            <li> Grosse Pointe </li>
-                                            <li> Grosse Pointe Park</li>
-                                            <li> Grosse Pointe Shores</li>
-                                            <li> Grosse Pointe Woods</li>
-                                            <li> Harper Woods</li>
-                                            <li> Livonia</li>
-                                            <li> Northville </li>
-                                            <li> Plymouth</li>
-                                            <li> Canton Township</li>
-                                            <li> Northville Township</li>
-                                            <li> Plymouth Township</li>
-                                        </ul>
-                                    </div>
-
-                                    <div class="service-list margin-bottom-md">
-
-                                        <strong>Parts of St. Clair County, MI </strong>
-                                        <ul>
-                                            <li>Call for more information</li>
-
-                                        </ul>
-                                    </div>
-
-                                    <div class="service-list margin-bottom-md">
-
-                                        <strong>Parts of Lapeer County, MI</strong>
-                                        <ul>
-                                            <li>Call for more information</li>
-
-                                        </ul>
-                                    </div>
-
-                                    <div class="service-list margin-bottom-md">
-
-                                        <strong>Parts of Livingston County, MI</strong>
-                                        <ul>
-                                            <li>Call for more information</li>
-
-                                        </ul>
-                                    </div>
-
-
-
-
-                                </div>
 
                             </div>
                         </div>
