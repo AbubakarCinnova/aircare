a2544dc3368e4affeb5bd457d6aabda26fd4f859
AbubakarCinnova <amahboob@cinnova.com>
2023-10-25 12:58:20 +0500

    specials

---
+++ specials.php
@@ -0,0 +1,32 @@
+<?php include_once('header.php') ?>
+<title>Advance AirCare | Specials </title>
+
+<?php include_once('menu.php') ?>
+</head>
+
+<body class="internal page  page-template page-template-home-php">
+
+    <div id="mainWrapper" style=" background-color:white;">
+        <div id="outerWrapper">
+            <div id="wrapper" class="container" style="background-color:white;">
+                <div class="container content-main"
+                    style="border-bottom-right-radius: 10px !important; border-bottom-left-radius: 10px !important; margin-bottom: 20px; padding-top: 30px;">
+                    <div class=" content-area specials-page">
+                        <div class="row">
+                            <div class="col-md-6">
+                                <img src="<?php echo $base_url ?>/img/10offdryer vent cleaning.jpg" alt="ad"
+                                    class="img-responsive">
+                            </div>
+                            <div class="col-md-6">
+                                <img src="<?php echo $base_url ?>/img/100offairduct cleaning.jpg" alt="ad"
+                                    class="img-responsive">
+                            </div>
+
+
+                        </div>
+                    </div>
+                </div><!-- new div end -->
+            </div>
+        </div>
+    </div>
+    <?php include_once('footer.php'); ?>
