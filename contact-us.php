9d0b9a2317c3d748da36c79226908dd0a0fdb08c
AbubakarCinnova <amahboob@cinnova.com>
2023-10-26 16:09:10 +0500

    updated contact photo

--- contact-us.php
+++ contact-us.php
@@ -272,7 +272,7 @@
         </div>
         <div class="col-md-6">
             <br><br>
-            <p><img src="<?php echo $base_url ?>/img/advanced-air-care-team.jpg" style=""></p>
+            <p><img src="<?php echo $base_url ?>/img/Advanced Air Care Macomb Mi.jfif" style=""></p>
         </div>
         <div class="col-sm-8 col-xs-12 margin-bottom-sm">
             <br>
